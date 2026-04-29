3b991e573d43c1567d55b7c549063295c8e434fb
M4TECH <fayzatamer66@gmail.com>
2026-04-30 00:07:25 +0300

    finalll2

--- Blood Donation Matcher/Form6.cs
+++ Blood Donation Matcher/Form6.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Linq;
+using static Person;
+using static Person.Patient;
 
 namespace Blood_Donation_Matcher
 {
@@ -56,7 +59,7 @@ namespace Blood_Donation_Matcher
 
         private void button2_Click(object sender, EventArgs e)
         {
-            
+
             string name = textBox2.Text;
             string phone = textBox3.Text;
             string age = numericUpDown1.Text;
@@ -65,23 +68,51 @@ namespace Blood_Donation_Matcher
             string city = comboBox2.Text;
             string userType = "";
 
+           
             if (radioButton1.Checked) { userType = "Donor (متبرع)"; }
             else if (radioButton2.Checked) { userType = "Patient (مريض)"; }
 
-            
-            if (name == "" || phone == ""||  age == "" || bloodType == "" || userType == "")
+            if (name == "" ||phone == "" || age == "" || bloodType == "" || userType == "")
     {
                 MessageBox.Show("من فضلك أكملي جميع البيانات الأساسية!", "تنبيه", MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
     else
             {
-                
-                string summary = $"تم تسجيل البيانات بنجاح!\nالاسم: {name}\nالنوع: {userType}\nالفصيلة: {bloodType}";
-                MessageBox.Show(summary, "نجاح", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                try
+                {
+                    
+                    BloodType bType = (BloodType)comboBox1.SelectedIndex;
 
-                
-                
-                
+                    string record = "";
+
+                    if (radioButton1.Checked) 
+                    {
+                        
+                        Donor d = new Donor(name, phone, int.Parse(age), address, bType, city  );
+                        record = "Donor|" + d.Name + "|" + d.Phone + "|" + d.Age + "|" + d.Address + "|" + d.City + "|" + d.bloodType;
+                    }
+                    else if (radioButton2.Checked) 
+                    {
+                        Patient p = new Patient(name, phone, int.Parse(age), address, bType, city );
+                        record = "Patient|" + p.Name + "|" + p.Phone + "|" + p.Age + "|" + p.Address + "|" + p.City + "|" + p.bloodType;
+                    }
+
+                    
+                    using (StreamWriter sw = new StreamWriter("UsersData.txt", true))
+                    {
+                        sw.WriteLine(record);
+                    }
+
+                    MessageBox.Show("تم الحفظ والربط بنجاح!");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("حدث خطأ: " + ex.Message);
+                }
+
+
+
+
             }
         }
         
@@ -209,6 +240,78 @@ namespace Blood_Donation_Matcher
             textBox2.BackColor = Color.White;  
         }
     }
-    }
-    
+}
 
+
+
+public class Person
+{
+    public string Name;
+    public string Phone;
+    public int Age;
+    public string Address;
+    public BloodType bloodType;
+    public string City;
+
+    public Person(string name, string phone, int age, string address, BloodType bloodType, string city)
+    {
+        this.Name = name;
+        this.Phone = phone;
+        this.Age = age;
+        this.Address = address;
+        this.bloodType = bloodType;
+        this.City = city;
+    }
+
+
+
+
+    public class Donor : Person
+    {
+        public Donor(string name, string phone, int age, string address, BloodType bloodType, string city )
+            : base(name, phone, age, address, bloodType, city)
+        {
+
+        }
+
+    }
+
+    public class Patient : Person
+    {
+        public Patient(string name, string phone, int age, string address, BloodType bloodType,string city)
+            : base(name, phone, age, address, bloodType, city)
+        {
+
+        }
+
+
+        public enum BloodType
+        {
+            A_Positive,
+            A_Negative,
+            B_Positive,
+            B_Negative,
+            AB_Positive,
+            AB_Negative,
+            O_Positive,
+            O_Negative
+        }
+
+
+        public class PersonManager
+        {
+            private Person[] people = new Person[100];
+            private int count = 0;
+
+
+            public void AddPerson()
+            {
+                if (count < 100)
+                    count++;
+            }
+
+        }
+    }
+}
+
+        
