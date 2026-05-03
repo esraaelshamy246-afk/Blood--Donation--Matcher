26754e881b621d2a0c254b261442ebd9fafd77d0
HP <HP@DESKTOP-ABOT1VN>
2026-05-03 19:21:30 +0300

    hgug,;

--- Blood Donation Matcher/Form4.Designer.cs
+++ Blood Donation Matcher/Form4.Designer.cs
@@ -28,19 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FeedbackForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Maroon;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(61, 153);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(519, 700);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Maroon;
+            this.button1.Location = new System.Drawing.Point(111, 588);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 33);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Submit ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(40, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(169, 22);
+            this.label6.TabIndex = 8;
+            this.label6.Text = " Your Blood Type  :";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(36, 197);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(169, 22);
+            this.label5.TabIndex = 7;
+            this.label5.Text = " Your Blood Type  :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "A+",
+            "A-",
+            "B+",
+            "B-",
+            "AB+",
+            "AB-",
+            "O+",
+            "O-"});
+            this.comboBox1.Location = new System.Drawing.Point(218, 195);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(253, 24);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(44, 310);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(428, 232);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(218, 135);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 22);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 233);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(40, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Your Name :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Snow;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(173, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Your Feedback ";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Maroon;
+            this.panel2.Controls.Add(this.flowLayoutPanel1);
+            this.panel2.Location = new System.Drawing.Point(610, 54);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1106, 843);
+            this.panel2.TabIndex = 10;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(21, 21);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1056, 778);
+            this.flowLayoutPanel1.TabIndex = 0;
             // 
             // FeedbackForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1728, 1014);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "FeedbackForm";
             this.Text = "Feedback";
+            this.Load += new System.EventHandler(this.FeedbackForm_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
     }
 }
--- Blood Donation Matcher/Form6.cs
+++ Blood Donation Matcher/Form6.cs
@@ -1,232 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using Blood_Donation_Matcher;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using System.Xml.Linq;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-
-
-
-namespace Blood_Donation_Matcher
+﻿public class Person
 {
-    public partial class RegisterForm : Form
+    public string Name { get; set; }
+    public string Phone { get; set; }
+    public string Age { get; set; } // جعلناه string ليتوافق مع استخدامك في BloodRequest
+    public string Gender { get; set; }
+    public string Address { get; set; }
+    public string City { get; set; }
+    public string BloodType { get; set; } // تم تغييره لـ string لتسهيل التعامل مع الـ ComboBox
+
+    // المشد (Constructor) الذي تستخدمه فئة BloodRequest
+    public Person(string name, string phone, string age, string gender)
     {
-        public RegisterForm()
-        {
-            InitializeComponent();
-        }
+        Name = name;
+        Phone = phone;
+        Age = age;
+        Gender = gender;
+    }
 
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void radioButton1_CheckedChanged(object sender, EventArgs e)
-        {
-            if (radioButton1.Checked)
-            {
-                radioButton2.Checked = false;
-            }
-        }
-
-        private void radioButton2_CheckedChanged(object sender, EventArgs e)
-        {
-            if (radioButton2.Checked)
-            {
-                radioButton1.Checked = false;
-            }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog ofd = new OpenFileDialog();
-
-            if (ofd.ShowDialog() == DialogResult.OK)
-            {
-                textBox2.Text = ofd.FileName;
-            }
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
-            string name = textBox2.Text;
-            string phone = textBox3.Text;
-            string age = numericUpDown1.Text;
-            string address = textBox5.Text;
-            string bloodType = comboBox1.Text;
-            string city = comboBox2.Text;
-            string userType = "";
-
-
-
-            if (radioButton1.Checked) { userType = "Donor (متبرع)"; }
-            else if (radioButton2.Checked) { userType = "Patient (مريض)"; }
-
-            if (name == "" || phone == "" || age == "" || bloodType == "" || userType == "")
-            {
-                MessageBox.Show("من فضلك أكملي جميع البيانات الأساسية!", "تنبيه", MessageBoxButtons.OK, MessageBoxIcon.Warning);
-            }
-            else
-            {
-                try
-                {
-                    MessageBox.Show("تم الحفظ والربط بنجاح!");
-                }
-                catch (Exception ex)
-                {
-                    MessageBox.Show("حدث خطأ: " + ex.Message);
-                }
-
-            }
-        }
-
-
-        private void RegisterForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox2_MouseEnter(object sender, EventArgs e)
-        {
-            textBox2.BackColor= Color.LightGray;
-        }
-        private void textBox2_MouseLeave(object sender, EventArgs e)
-        {
-            textBox2.BackColor = Color.White;
-        }
-
-        private void numericUpDown1_MouseDown(object sender, MouseEventArgs e)
-        {
-            numericUpDown1.BackColor = Color.White;
-        }
-
-        private void numericUpDown1_MouseClick(object sender, MouseEventArgs e)
-        {
-            numericUpDown1.BackColor = Color.White;
-        }
-
-        private void numericUpDown1_MouseUp(object sender, MouseEventArgs e)
-        {
-            numericUpDown1.BackColor = Color.White;
-        }
-
-        private void textBox4_MouseEnter(object sender, EventArgs e)
-        {
-            textBox3.BackColor= Color.LightGray;
-        }
-
-        private void textBox4_MouseLeave(object sender, EventArgs e)
-        {
-            textBox3.BackColor= Color.White;
-        }
-
-        private void comboBox1_MouseEnter(object sender, EventArgs e)
-        {
-            comboBox1.BackColor= Color.LightGray;
-        }
-
-        private void comboBox1_MouseLeave(object sender, EventArgs e)
-        {
-            comboBox1.BackColor = Color.White;
-        }
-
-        private void textBox6_MouseLeave(object sender, EventArgs e)
-        {
-            textBox6.BackColor = Color.White;
-        }
-
-        private void textBox6_MouseEnter(object sender, EventArgs e)
-        {
-            textBox6.BackColor= Color.LightGray;
-        }
-
-        private void comboBox2_MouseEnter(object sender, EventArgs e)
-        {
-            comboBox2.BackColor = Color.LightGray;
-        }
-
-        private void comboBox2_MouseLeave(object sender, EventArgs e)
-        {
-            comboBox2.BackColor = Color.White;
-        }
-
-        private void textBox5_MouseEnter(object sender, EventArgs e)
-        {
-            textBox5.BackColor= Color.LightGray;
-        }
-
-        private void textBox5_MouseLeave(object sender, EventArgs e)
-        {
-            textBox5.BackColor= Color.White;
-        }
-
-        private void button1_MouseEnter(object sender, EventArgs e)
-        {
-            button1.BackColor = Color.LightGray;
-        }
-
-        private void button1_MouseLeave(object sender, EventArgs e)
-        {
-            button1.BackColor = Color.White;
-        }
-
-        private void textBox2_MouseLeave_1(object sender, EventArgs e)
-        {
-            textBox2.BackColor = Color.White;  
-        }
+    // المشد الخاص بعملية التسجيل الكاملة
+    public Person(string name, string phone, string age, string address, string bloodType, string city)
+    {
+        Name = name;
+        Phone = phone;
+        Age = age;
+        Address = address;
+        BloodType = bloodType;
+        City = city;
     }
 }
 
-
-//class person 
-public class Person
-{
-    public string Name;
-    public string Phone;
-    public int Age;
-    public string Address;
-    public BloodType bloodType;
-    public string City;
-
-    public Person(string name, string phone, int age, string address, BloodType bloodType, string city)
-    {
-        this.Name = name;
-        this.Phone = phone;
-        this.Age = age;
-        this.Address = address;
-        this.bloodType = bloodType;
-        this.City = city;
-    }
-        //enum  
-        public enum BloodType
-        {
-            A_Positive,
-            A_Negative,
-            B_Positive,
-            B_Negative,
-            AB_Positive,
-            AB_Negative,
-            O_Positive,
-            O_Negative
-        } 
-}
-
-        
